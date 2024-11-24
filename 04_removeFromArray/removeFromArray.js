3039ebbc7b3f3d52513f7758c254abdd894f5129
Stanislav Zelinsky <51513041+lazytryhard96@users.noreply.github.com>
2024-11-24 19:04:46 +0100

    feat: Complete js exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
 const removeFromArray = function() {
+    if (arguments.length < 2){
+        return;
+    }
+    const originalArray = arguments[0];
+    const argumentsToMatch = [...arguments].slice(1);
+    return originalArray.filter((element) => !argumentsToMatch.includes(element));
 };
 
 // Do not edit below this line
