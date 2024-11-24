79305e6e7b8f3bd3308b5c15ba971c6d87b9d63a
Stanislav Zelinsky <51513041+lazytryhard96@users.noreply.github.com>
2024-11-24 19:38:53 +0100

    feat: Complete js exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
 const sumAll = function() {
-
+    if (arguments.length != 2 || !validateArguments(arguments)){
+        return "ERROR";
+    }
+    const lesser = Math.min(arguments[0],arguments[1]);
+    const higher = Math.max(arguments[0],arguments[1]); 
+    return (lesser + higher) * ((higher - lesser + 1) / 2);
 };
+
+const validateArguments = function() {
+    let valid = true;
+    [...arguments[0]].forEach(argument => {
+        if (!Number.isInteger(argument) || argument < 0){
+            valid = false;
+            return;
+        }
+    });
+    return valid;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
